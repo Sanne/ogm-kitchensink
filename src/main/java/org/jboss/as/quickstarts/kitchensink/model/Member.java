a7d673049d43909887f26450a774d5301d84379f
Hardy Ferentschik <hardy@ferentschik.de>
2011-10-10 14:27:47 +0200

    adding @Indexed and switching to different id generator

--- src/main/java/org/jboss/as/quickstarts/kitchensink/model/Member.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/model/Member.java
@@ -1,7 +1,6 @@
 package org.jboss.as.quickstarts.kitchensink.model;
 
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,65 +13,75 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
+@Indexed
 @XmlRootElement
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class Member implements Serializable {
-   /** Default value included to remove warning. Remove or modify at will. **/
-   private static final long serialVersionUID = 1L;
+	/**
+	 * Default value included to remove warning. Remove or modify at will. *
+	 */
+	private static final long serialVersionUID = 1L;
 
-   @Id
-   @GeneratedValue
-   private Long id;
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String id;
 
-   @NotNull
-   @Size(min = 1, max = 25)
-   @Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
-   private String name;
+	@NotNull
+	@Size(min = 1, max = 25)
+	@Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
+	@Field
+	private String name;
 
-   @NotNull
-   @NotEmpty
-   @Email
-   private String email;
+	@NotNull
+	@NotEmpty
+	@Email
+	@Field
+	private String email;
 
-   @NotNull
-   @Size(min = 10, max = 12)
-   @Digits(fraction = 0, integer = 12)
-   @Column(name = "phone_number")
-   private String phoneNumber;
+	@NotNull
+	@Size(min = 10, max = 12)
+	@Digits(fraction = 0, integer = 12)
+	@Column(name = "phone_number")
+	@Field
+	private String phoneNumber;
 
-   public Long getId() {
-      return id;
-   }
+	public String getId() {
+		return id;
+	}
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-   public String getName() {
-      return name;
-   }
+	public String getName() {
+		return name;
+	}
 
-   public void setName(String name) {
-      this.name = name;
-   }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-   public String getEmail() {
-      return email;
-   }
+	public String getEmail() {
+		return email;
+	}
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-   public String getPhoneNumber() {
-      return phoneNumber;
-   }
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
 
-   public void setPhoneNumber(String phoneNumber) {
-      this.phoneNumber = phoneNumber;
-   }
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 }
