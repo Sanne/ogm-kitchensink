ebe5fca53f2b64c521370b4bb44d654951fd61a0
Sanne Grinovero <sanne@hibernate.org>
2012-06-16 12:22:05 +0100

    Previous factories should be @ApplicationScoped

--- src/main/java/org/jboss/as/quickstarts/kitchensink/util/Resources.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/util/Resources.java
@@ -18,6 +18,7 @@ package org.jboss.as.quickstarts.kitchensink.util;
 
 import java.util.logging.Logger;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
@@ -54,11 +55,13 @@ public class Resources {
     }
 
     @Produces
+    @ApplicationScoped
     public SearchFactory getSearchFactory() {
         return getFullTextEntityManager().getSearchFactory();
     }
 
     @Produces
+    @ApplicationScoped
     public QueryBuilder getMemberQueryBuilder() {
         return getSearchFactory().buildQueryBuilder().forEntity(Member.class).get();
     }
